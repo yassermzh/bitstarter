78a13080fc6c08d53cde6cd7283a0d943bd82d71
Yasser Mohammadzadeh <yassermzh@gmail.com>
2013-07-05 19:28:38 +0430

    read from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+    response.send(fs.readFileSync('index.html').toString());
+   
+   //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
